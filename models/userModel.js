b27d1de2134aec3268d7feeaea70092a014d69ef
Ankit Nayan <ankitnayan83@gmail.com>
2023-06-03 16:48:01 +0530

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -36,6 +36,10 @@ const userSchema = new mongoose.Schema(
          type: Number,
          default: 0,
       },
+      balance: {
+         type: Number,
+         default: 0,
+      },
    },
    { timestamps: true }
 );
