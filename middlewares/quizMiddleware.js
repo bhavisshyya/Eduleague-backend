5b5a5b6a8124ac499f6669b0b754bc5c54d9102c
Ankit Nayan <ankitnayan83@gmail.com>
2023-06-27 18:09:16 +0530

    commit

--- middlewares/quizMiddleware.js
+++ middlewares/quizMiddleware.js
@@ -3,11 +3,9 @@ import Quiz from "../models/quizModel.js";
 export const checkParticipant = async (req, res, next) => {
    const id = req.params.id;
    const quiz = await Quiz.findById(id).populate("participants");
-   //    console.log(quiz);
    const participants = quiz.participants;
 
    participants.forEach((item) => {
-      //   console.log(item.user);
       if (item.user == req.user.userId) {
          return next("You have already participated in this quiz");
       }
