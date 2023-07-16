e849752affec2b687077ab15620a42fa2d2a30ea
bhavisshyya <bhavisshyya@gmail.com>
2023-07-17 01:58:40 +0530

    commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,25 @@
 import Kyc from "../models/kycModel.js";
 import User from "../models/userModel.js";
 
-// export const updateUser = async (req, res, next) => {
-//    //this function is only to update user details
+export const updateUser = async (req, res, next) => {
+   //this function is only to update user details
+   const { fName, lName } = req.body;
+   if (!fName || !lName) return next("No field can be left empty");
 
-// };
+   const user = await User.findOne({ _id: req.user.userId });
+
+   // user.email = email;
+   user.fName = fName;
+   // user.location = location;
+   user.lName = lName;
+   await user.save();
+
+   res.status(200).json({
+      message: "user updated successfully",
+      success: true,
+      user,
+   });
+};
 
 export const getUser = async (req, res, next) => {
    const user = await User.findById({ _id: req.params.id });
